08a597407126fa415b2410b546a5b5cb40a78b79
HamzaBoukaftane <hamzaboukaftane1996@outlook.fr>
2023-07-06 23:20:32 -0400

    Changed prompt format
    Changed the prompt to better format

--- test2/routes/index.js
+++ test2/routes/index.js
@@ -1,22 +1,37 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 const { Configuration, OpenAIApi } = require("openai");
 require('dotenv').config()
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY
+  
 });
 
 const openai = new OpenAIApi(configuration);
 
-async function runCompletion (strategy, takeloss, takeprofit, typetrade) {
-  var content = `give me only the script on Java for ${strategy} with a take loss of ${takeloss} and a take profit of ${takeprofit} using ${typetrade}`
+async function runCompletion (broker, instrument, leverage, entry, exit, risk) {
+  let strategy = 
+  ` I want to make a trading bot working live on my trading account with ${broker}.
+  The trading bot will trade on the ${instrument} market. It is important to note
+  that I am using ${leverage} leverage. 
+  Here is my entry strategy : 
+  ${entry}
+  Here is my exit strategy : 
+  ${exit}
+  Here is the risk I am willing to take on each trade : 
+  ${risk}
+  Generate the complete code in javascript that implement my strategy with the
+  information i have given you. The code should be working once I have provided
+  the API key from my account.
+  Make sure to place stop loss according to the risk management on each order and
+  to have a method runBot.
+  Implement all the logics.`
   const completion = await openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: "Hello How you doing?",
-  max_tokens:4000
-  });
-  //console.log(completion.data.choices[0].text);
+    model: "text-davinci-003",
+    prompt: strategy,
+    max_tokens:8000
+   });
   return completion.data.choices[0].text;
 }
 
@@ -26,23 +41,25 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/sent', async function(req, res, next) {
-  const strategy = req.body.strategy;
-  const minbudget = req.body.minbudget;
-  const maxbudget = req.body.maxbudget;
-  const typetrade = req.body.typetrade;
-  const respond = await runCompletion(strategy,minbudget,maxbudget,typetrade);
-  res.render('index',{msg: "SUCCES!", Respond:respond});
+  const broker = req.body.broker;
+  const instrument = req.body.instrument;
+  const leverage = req.body.leverage;
+  const entry = req.body.entry;
+  const exit = req.body.exit;
+  const risk = (req.body.risk)=== null ? req.body.risk : "3% risk on margin";
+  const respond = await runCompletion(broker, instrument, leverage, entry, exit, risk);
+  res.render('index', {msg: "SUCCES!", Respond:respond});
 });
 
-router.post('/execute-code', async function(req, res, next) {
-  const code = req.body.code;
-  try {
-    // Evaluate the code using eval()
-    const result = eval(code);
-    res.status(200).json({ result });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-})
+// router.post('/execute-code', async function(req, res, next) {
+//   const code = req.body.code;
+//   try {
+//     // Evaluate the code using eval()
+//     const result = eval(code);
+//     res.status(200).json({ result });
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// })
 
 module.exports = router;
